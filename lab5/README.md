8aca30c8a3c312aae0363642bf1138f34286b561
Arslan <alan.aquinasss@gmail.com>
2024-02-28 00:51:37 +0600

    init

--- lab5/README.md
+++ lab5/README.md
@@ -1,4 +1,4 @@
-# OnlineShopNeDoKaspiAngular
+# Lab5
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.2.1.
 
--- lab5/src/app/app.component.ts
+++ lab5/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'online-shop-NeDoKaspi-angular';
+  title = 'lab5';
 }
