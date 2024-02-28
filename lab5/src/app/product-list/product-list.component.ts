45b30cddc392390a69eea0ba0950463b75f133b0
Arslan <alan.aquinasss@gmail.com>
2024-02-28 18:58:52 +0600

    finish lab 5

--- lab5/src/app/product-list/product-list.component.ts
+++ lab5/src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 import { Product, products } from '../products';
 
 @Component({
@@ -6,24 +7,64 @@ import { Product, products } from '../products';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   products: Product[] = products;
   searchInput: string = '';
+
+  constructor(
+      private route: ActivatedRoute,
+  ) {}
+  ngOnInit() {
+
+
+    const routeParams = this.route.snapshot.paramMap;
+    const categoryIdFromRoute = Number(routeParams.get('categoryId'));
+
+    this.products = products.filter(products => products.categoryId === categoryIdFromRoute);
+  }
 
   get filteredProducts(): Product[] {
     const searchTerm = this.searchInput.toLowerCase();
     return this.products.filter(product => product.name.toLowerCase().includes(searchTerm));
   }
 
-  shareWA(productName: string, productLink: string) {
+  share(productName: string, productLink: string) {
     const shareMessage = `Check out this product: ${productName} - ${productLink}`;
     const whatsappLink = `https://api.whatsapp.com/send?text=${encodeURIComponent(shareMessage)}`;
     window.location.href = whatsappLink;
   }
 
-  shareTG(productName: string, productLink: string) {
+  sharetg(productName: string, productLink: string) {
     const shareMessage = `Check out this product: ${productName} - ${productLink}`;
-    const telegramLink = `https://t.me/share/url?url=${encodeURIComponent(shareMessage)}`;
-    window.location.href = telegramLink;
+    const whatsappLink = `https://t.me/share/url?url=${encodeURIComponent(shareMessage)}`;
+    window.location.href = whatsappLink;
+  }
+
+  nextImage(product: Product) {
+    if (product.img.length > 1) {
+      product.currentImageIndex = (product.currentImageIndex + 1) % product.img.length;
+    }
+  }
+  like(product: Product) {
+    product.likes++;
+  }
+  prevImage(product: Product) {
+    if (product.img.length > 1) {
+      product.currentImageIndex =
+        (product.currentImageIndex - 1 + product.img.length) % product.img.length;
+    }
+  }
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+  delete(product: Product) {
+    const index = this.products.indexOf(product);
+  
+    if (index !== -1) {
+      this.products.splice(index, 1);
+      window.alert('Product has been deleted');
+    } else {
+      window.alert('Product not found');
+    }
   }
 }
